fd024fedc24529989741e54dbfc49496329c4675
Fazliddin <fmamutkhanov@gmail.com>
2024-10-24 10:00:00 +0900

    october 24

--- arrays/merge_sorted_array/solution.py
+++ arrays/merge_sorted_array/solution.py
@@ -5,7 +5,6 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
      
         last = m + n - 1
-
         while n > 0:
             if m > 0 and nums1[m-1] > nums2[n-1]:
                 nums1[last] = nums1[m-1]
