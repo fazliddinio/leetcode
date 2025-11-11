ce1485b8d1bae66e65a76501a5ce806b1600575c
Fazliddin <fmamutkhanov@gmail.com>
2025-11-11 10:45:19 +0900

    move zeroes solution

---
+++ two_pointers/move_zeroes/test_solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> int:
+        
+        left = 0
+        
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
